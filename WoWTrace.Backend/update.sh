c4b9299a3fc1f6f3e318b9181717626400a53062
root <root@_HOSTNAME_>
2022-01-30 17:17:51 +0100

    Add +x to update.sh

--- WoWTrace.Backend/update.sh
+++
@@ -1,6 +0,0 @@
-﻿# git
-git update
-git submodule update --init --recursive
-
-# dotnet
-dotnet build -c Release
---
+++ update.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+
+# git
+git pull
+git submodule update --init --recursive
+
+# dotnet
+dotnet build -c Release
+
